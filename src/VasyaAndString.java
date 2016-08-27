2060827c4ea9fede0fb5922c949183b8143ab384
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-27 04:06:27 +0200

    Add working solution for VasyaAndString of http://codeforces.com/contest/676

--- src/VasyaAndString.java
+++ src/VasyaAndString.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
 
 /**
  * Created by mohannadhassan on 5/25/16.
@@ -18,75 +16,33 @@ public class VasyaAndString {
 
         String s = br.readLine();
 
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<VasyaAndStringPair> sorte = new ArrayList<>();
+        int l = 0, r = 0;
+        int max = 0;
+        int as = 0, bs = 0;
 
-        for (int i = 0; i < n; i++) {
-            int j;
-            for (j = i + 1; j < n && s.charAt(i) == s.charAt(j); j++);
-
-            int length = j - i;
-            list.add(length);
-            sorte.add(new VasyaAndStringPair(length, list.size() - 1));
-        }
-
-        int max = 1;
-        for (VasyaAndStringPair pair :
-                sorte) {
-            int index = pair.index;
-
-            int left = index - 1;
-            int right = index + 1;
-
-            int temp = k;
-            int total = pair.count;
-
-            if (total + k <= max) {
-                break;
-            }
-            while (temp > 0 && (left >= 0 || right < list.size())) {
-                int toAdd = 0;
-                int blah = -1;
-                if (left > 0 && (right >= list.size() || list.get(left) > list.get(right))) {
-                    toAdd = list.get(left);
-                    left -= 2;
-                    blah = left;
-                }
-                else if (right < list.size() && (left < 0 || list.get(right) > list.get(left))){
-                    toAdd = list.get(right);
-                    right += 2;
-                    blah = right;
-                }
-                if (toAdd > temp) {
-                    toAdd = temp;
-                }
-                temp -= toAdd;
-                total += toAdd;
-
-                if (blah >= 0 && blah < list.size()) {
-                    total += list.get(blah);
-                }
+        while (r < n) {
+            while (r < n && (as <= k || bs <= k)) {
+                if (s.charAt(r) == 'a')
+                    as++;
+                else
+                    bs++;
+                r++;
             }
 
-            if (total > max) {
-                max = total;
+            int len = r - l - (r < n || !(as <= k || bs <= k) ? 1 : 0);
+            if (len > max) {
+                max = len;
             }
+            if (r < n)
+                while (l < r && (as > k && bs > k)) {
+                    if (s.charAt(l) == 'a')
+                        as--;
+                    else
+                        bs--;
+                    l++;
+                }
         }
         System.out.println(max);
-    }
-}
-
-class VasyaAndStringPair implements Comparator<VasyaAndStringPair>{
-    int count;
-    int index;
-
-    public VasyaAndStringPair(int count, int index) {
-        this.count = count;
-        this.index = index;
-    }
-
-    @Override
-    public int compare(VasyaAndStringPair o1, VasyaAndStringPair o2) {
-        return o2.count - o1.count;
+        System.exit(0);
     }
 }
