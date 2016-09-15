dd976d880b3d0bf7d75626c92f794aadfbecab50
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-09-15 02:15:03 +0200

    Remove unused code in SonyaAndQueries

--- src/SonyaAndQueries.java
+++ src/SonyaAndQueries.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 
 /**
  * Created by MohannadHassanPersonal on 9/13/16.
